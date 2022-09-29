3c717fd4b4f3b5916898c4bd53c719fdc0241cdb
Juan Villarreal <juanf.villarrealrqvirtual.edu.co>
2022-09-29 15:50:47 -0500

    se ha agregado nuevos cambios, se ha creado la clase

--- parcial2.py
+++ parcial2.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+
+#class funcion(){
+        #}
 """
 
 
 """
 
+import matplotlib.pyplot as mpl
 
+pi = 3.1415926535897
 
 def factorial (n):
     f = 1
@@ -12,45 +17,51 @@ def factorial (n):
         f = f*i
     return (f)
 
-
-
-
-
-def func_e(x, n):
-    e_approx = 0
-    for i in range(n):
-        e_approx += x**i/factorial(i)
+class Funcion:
     
-    return e_approx
-
-
-def func_cos(x, n):
-    cos_approx = 0
-    for i in range(n):
-        coef = (-1)**i
-        num = x**(2*i)
-        denom = factorial(2*i)
-        cos_approx += ( coef ) * ( (num)/(denom) )
+    def __init__(self, func, x, n):
+            self.func = func
+            self.x = x
+            self.n = n
+#-----------------------------------------------------------------------------            
+    def func_e( x, n):
+        e_approx = 0
+        for i in range(n):
+            e_approx += x**i/factorial(i)
+        return e_approx
+#-----------------------------------------------------------------------------
+    def func_sen(self, x, n):
+        sen_approx = 0 
+        for i in range(n):
+            coef = x**(2*i +1)
+            num =  (-1)**i
+            denom = factorial(2*i+1)
+            sen_approx += ( coef ) * ( (num)/(denom) )
+        return sen_approx
+#-----------------------------------------------------------------------------           
+    def func_cos(self, x, n):
+        cos_approx = 0
+        for i in range(n):
+            coef = (-1)**i
+            num = x**(2*i)
+            denom = factorial(2*i)
+            cos_approx += ( coef ) * ( (num)/(denom) )
     
-    return cos_approx
+        return cos_approx
 
 
-"""
-class aproximaciondefunciones():
-     
-    def __init___(self, x, precision):
-        self.x = x
-        self.precision = precision 
-        
-   
-"""        
-        
-        
-        
-        
-        
-        
-        
-        
 
+e1= Funcion.func_e( 5, 19)
+
+
+print(e1)
+
+#----------------------------------------------------------------------------
+
+
+
+        
+        
+        
+        
 
