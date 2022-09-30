3588c297c2a015ccb9046629a9491fce4a5029d2
Juan Villarreal <juanf.villarrealrqvirtual.edu.co>
2022-09-30 14:26:06 -0500

    se han hecho nuevos cambios, por ejemplo se vincularon los metodos de las aproximaciones de las funciones a métdos para poder graficar esas aproximaciones teniendo en cuenta que la presición es un atributo que ingresa el programador, falta que este sea un input para ir terminando la parte de las gráficas, para termianr tamíén faltan arreglos a las gráficas y comentarios respectivos a cada proceso.

--- parcial2.py
+++ parcial2.py
@@ -6,7 +6,7 @@
 
 
 """
-
+import numpy as np
 import matplotlib.pyplot as mpl
 
 pi = 3.1415926535897
@@ -24,13 +24,13 @@ class Funcion:
             self.x = x
             self.n = n
 #-----------------------------------------------------------------------------            
-    def func_e( x, n):
+    def func_e(x, n):
         e_approx = 0
         for i in range(n):
             e_approx += x**i/factorial(i)
         return e_approx
 #-----------------------------------------------------------------------------
-    def func_sen(self, x, n):
+    def func_sen(x, n):
         sen_approx = 0 
         for i in range(n):
             coef = x**(2*i +1)
@@ -39,7 +39,7 @@ class Funcion:
             sen_approx += ( coef ) * ( (num)/(denom) )
         return sen_approx
 #-----------------------------------------------------------------------------           
-    def func_cos(self, x, n):
+    def func_cos(x, n):
         cos_approx = 0
         for i in range(n):
             coef = (-1)**i
@@ -51,17 +51,53 @@ class Funcion:
 
 
 
-e1= Funcion.func_e( 5, 19)
 
 
-print(e1)
+sen1= Funcion.func_sen(1.5, 10)
+
+cos1= Funcion.func_cos(1.5, 10)
+
+
+
 
 #----------------------------------------------------------------------------
 
+x= np.linspace(0, 5, 100)
+ep= np.exp(x)
+cos=np.cos(x)
+sin=np.sin(x)
+
+e1= Funcion.func_e( x, 3)      
+
+sen1= Funcion.func_sen(x, 20)
+
+cos1= Funcion.func_cos(x, 3)
+
+mpl.title("Aproximaciones a las funciones")
+mpl.subplot(1,1,1)
+mpl.title("Aproximaciones a las funciones")
+mpl.plot(x, e1)
+mpl.plot(x, ep)
+mpl.xlabel("X")
+mpl.ylabel("Aproximación")
+        
+
+#-------------------------------------------------------------------------------
 
 
-        
-        
-        
-        
+mpl.subplot(1,1,1)
+
+mpl.plot(x, sen1)
+mpl.plot(x, sin)
+#------------------------------------------------------------------------------
+
+mpl.subplot(1,1,1)
+
+mpl.plot(x, cos1)
+mpl.plot(x, cos)
+
+mpl.show()
+
+
+
 
